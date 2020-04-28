60fc5e57e5d003df6f19b57f69dba658dcd89e1d
leviminch04 <minchl@wlhs.wlwv.k12.or.us>
2020-04-27 20:02:56 -0700

    Finish Week 3

---
+++ src/Week3/FRQ2/Airport.java
@@ -0,0 +1,17 @@
+package Week3.FRQ2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Airport {
+    public List<Airplane> carrierMaintenance(String carrier, int currMonth, int currYear)
+    {
+        ArrayList<Airplane> needMaint = new ArrayList<>();
+        for (int i = 0; i < airplanes.size(); i++) {
+            if(airplanes.get(i).getCarrier().equals(carrier))
+                if(airplanes.get(i).maintenanceNeeded())
+                    needMaint.add(airplanes.get(i));
+        }
+        return needMaint;
+    }
+}
